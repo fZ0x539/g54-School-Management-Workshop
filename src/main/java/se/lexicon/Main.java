da719b4faeeb3c0fa23e9574af6fdc97e47ab060
Daniel <daka98001@gmail.com>
2025-02-12 09:43:41 +0100

    Implemented StudentDaoList.java

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,12 +1,30 @@
 package se.lexicon;
 
+import se.lexicon.data_access.StudentDaoList;
+
 public class Main {
     public static void main(String[] args) {
+
         Student student1 = new Student("Daniel", "daka@gmail.com", "Järnvägsgatan 1337");
         Course course1 = new Course("Java 101");
-        course1.register(student1);
-        System.out.println(course1.getStudentList());
-        course1.unregister(student1);
-        System.out.println(course1.getStudentList());
+
+        StudentDaoList studentDao = new StudentDaoList();
+        studentDao.saveStudent(student1);
+        System.out.println(studentDao.findByName("Daniel"));
+        System.out.println(studentDao.findByName("Daniele")); //Empty list
+        System.out.println(studentDao.findById(1));
+        System.out.println(studentDao.findById(0)); // null
+        System.out.println(studentDao.findByEmail("daka@gmail.com"));
+        System.out.println(studentDao.findByEmail("jaha@gmail.com")); // null
+
+        System.out.println(studentDao.findAll());
+
+        studentDao.deleteStudent(student1);
+        System.out.println(studentDao.findAll());
+
+//        course1.register(student1);
+//        System.out.println(course1.getStudentList());
+//        course1.unregister(student1);
+//        System.out.println(course1.getStudentList());
     }
 }
--- src/main/java/se/lexicon/data_access/StudentDaoList.java
+++ src/main/java/se/lexicon/data_access/StudentDaoList.java
@@ -4,38 +4,77 @@ import se.lexicon.Student;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class StudentDaoList implements StudentDao {
     private static List<Student> students = new ArrayList<>();
 
-
     @Override
     public Student saveStudent(Student student) {
+        if (Objects.nonNull(student)) {
+            if (!students.contains(student)) {
+                students.add(student);
+                System.out.println("Added " + student.getName() + " to StudentDaoList");
+                return student;
+            } else
+                System.out.println(student.getName() + " is already registered to StudentDaoList");
+        }
         return null;
     }
 
+    //Returns the student object if found, else null
     @Override
     public Student findByEmail(String email) {
-        return null;
+        Student tempStudent = null;
+        for (Student student : students) {
+            if (student.getEmail().equals(email))
+                tempStudent = student;
+        }
+        if(tempStudent == null)
+            System.out.println("Couldn't find any student with that email in the StudentDaoList");
+        return tempStudent;
     }
+
 
     @Override
     public List<Student> findByName(String name) {
-        return List.of();
+        List<Student> studentsWithMatchingName = new ArrayList<>();
+        for (Student student : students) {
+            if (student.getName().equalsIgnoreCase(name))
+                studentsWithMatchingName.add(student);
+        }
+        if(studentsWithMatchingName.isEmpty())
+            System.out.println("Couldn't find any student with that name in the StudentDaoList");
+        return studentsWithMatchingName;
     }
 
     @Override
     public Student findById(int id) {
-        return null;
+        Student tempStudent = null;
+        for (Student student : students) {
+            if (student.getId() == id)
+                tempStudent = student;
+        }
+        if(tempStudent == null)
+            System.out.println("Couldn't find any student with that id in the StudentDaoList");
+        return tempStudent;
     }
 
     @Override
     public List<Student> findAll() {
-        return List.of();
+        return students;
     }
 
     @Override
     public boolean deleteStudent(Student student) {
+        if (Objects.nonNull(student)) {
+            if (students.contains(student)) {
+                students.remove(student);
+                System.out.println("Removed " + student.getName() + " from StudentDaoList");
+                return true;
+            } else
+                System.out.println(student.getName() + " isn't registered to StudentDaoList");
+        }
         return false;
     }
 }
